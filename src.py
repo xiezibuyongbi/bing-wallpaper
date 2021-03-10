3c91b7eaec7d6c93ae15e64db2a2cb6595ca2b4f
xiezibuyongbi <59090566+xiezibuyongbi@users.noreply.github.com>
2021-03-10 20:58:09 +0800

    bug fixed, download pic rules modifed
    download the bing wallpaper only if the latest downloaded picture expired(over a day)

--- src.py
+++ src.py
@@ -5,12 +5,29 @@ import win32gui
 import win32con
 from urllib import request
 import os
+import time
+
+
+def flushDesktop(bgImgPath):
+    reg_key = win32api.RegOpenKeyEx(
+        win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
+    win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    win32gui.SystemParametersInfo(
+        win32con.SPI_SETDESKWALLPAPER, bgImgPath, win32con.SPIF_SENDWININICHANGE)
+
 
 # set your temp path to save background image here
 bgImgPath = "C:\\Users\\xiezibuyongbi\\Desktop\\temp\\today_background.bmp"
 
 # reserver the downloaded picture until next launch
-if os._exists(bgImgPath):
+if os.path.exists(bgImgPath):
+    modifiedDay = time.gmtime(os.path.getmtime(bgImgPath)).tm_mday
+    now = time.gmtime(time.time()).tm_mday
+    print(now-modifiedDay)
+    if (now-modifiedDay) < 1:
+        flushDesktop(bgImgPath)
+        os._exit(0)
     os.remove(bgImgPath)
 
 chromeOptions = Options()
@@ -25,10 +42,4 @@ with request.urlopen(bgImgUrl) as response:
     img.write(response.read())
     img.close()
 browser.quit()
-
-reg_key = win32api.RegOpenKeyEx(
-    win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
-win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
-win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
-win32gui.SystemParametersInfo(
-    win32con.SPI_SETDESKWALLPAPER, bgImgPath, win32con.SPIF_SENDWININICHANGE)
+flushDesktop(bgImgPath)
